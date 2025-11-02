32fed0fa98e3817a3104d047e6cedca399e0e891
RubenPop <rvmorago90@gmail.com>
2025-11-02 15:53:03 +0100

    feat: Añadida ruta GET /user para obtener el usuario autenticado

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\WebAuthController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('expenses', ExpenseController::class);
     Route::apiResource('categories', CategoryController::class);
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [WebAuthController::class, 'user']);
 });
 
     
